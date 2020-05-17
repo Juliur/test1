2ec78f491cd7b8d124aad96d4409fca5f208c807
Julia <juliayrkina@gmail.com>
2020-05-17 15:58:39 +0300

    gulpfile changed

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,1 +1,13 @@
-$(".trigger").on("click",function(){$(".popupForm").fadeIn(300)}),$(".popupForm, .close").on("click",function(){$(".popupForm").fadeOut(300)}),$(".show-alert").on("click",function(){$(".popupForm").fadeOut(500,function(){alert("DONE")})});
+$('.trigger').on('click', (function (){
+  $('.popupForm').fadeIn(300);
+}));
+
+$('.popupForm, .close').on('click', (function () { 
+  $('.popupForm').fadeOut(300); 
+}));
+
+$('.show-alert').on('click', (function () {
+  $('.popupForm').fadeOut(500, function(){
+    alert("DONE")
+  })}
+));
--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,14 @@ gulp.task('scss', function(){
   return gulp.src('app/scss/popup-style.scss')
               .pipe(autoprefixer(['last 5 versions', 'ie 8', 'ie 7'], { cascade: true }))
               .pipe(sass({outputStyle: 'compressed'})) 
-              .pipe(gulp.dest('app/css')) 
+              .pipe(gulp.dest('dist/css')) 
 });
 
 // Minify .js
 gulp.task('uglify', function(){
   return gulp.src('app/js/*.js')
               .pipe(uglify())
-              .pipe(gulp.dest('app/js'))
+              .pipe(gulp.dest('dist/js'))
 });
 
 // Minify html
@@ -42,15 +42,6 @@ gulp.task('img', function() {
               .pipe(gulp.dest('dist/img')); 
 });
 
-// Build Production Site
-gulp.task('build-dist', function(done) {
-  var buildCss = gulp.src('app/css/*.css')
-                      .pipe(gulp.dest('dist/css'));
- 
-  var buildJs = gulp.src('app/js/*.js')
-                    .pipe(gulp.dest('dist/js'));
-  done();
-});
 
 // Build Production Site with all updates
-gulp.task('build', gulp.series('scss', 'uglify', 'minify-html', 'img' ,'build-dist'));
+gulp.task('build', gulp.series('scss', 'uglify', 'minify-html', 'img'));
